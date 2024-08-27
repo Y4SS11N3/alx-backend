b3375ed3cb60302962fdd2ae548c334312d61d9b
YASSINE <contact@yassine.fun>
2024-08-27 07:32:25 -0700

    0x02. i18n

---
+++ 0x02-i18n/6-app.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+"""Flask app with Babel setup, user locale preference, and mock login"""
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+class Config:
+    """Config class for Flask app"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+def get_user():
+    """Returns a user dictionary or None if ID cannot be found"""
+    login_id = request.args.get('login_as')
+    if login_id:
+        return users.get(int(login_id))
+    return None
+
+@app.before_request
+def before_request():
+    """Find a user if any, and set it as a global on flask.g.user"""
+    g.user = get_user()
+
+@babel.localeselector
+def get_locale():
+    """Determine the best match with our supported languages"""
+    locale = request.args.get('locale')
+    if locale and locale in app.config['LANGUAGES']:
+        return locale
+    
+    if g.user and g.user['locale'] in app.config['LANGUAGES']:
+        return g.user['locale']
+    
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+    
+@app.route('/', strict_slashes=False)
+def index():
+    """Render the index page"""
+    return render_template('6-index.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
